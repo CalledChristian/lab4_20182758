1c62b9be32fbe5fe918a6b4b4cff0a03f48bd487
Lenovo <calledchristian79@gmail.com>
2023-04-18 21:56:44 -0500

    commit 3

--- src/main/java/com/example/demo/controller/InicioController.java
+++ src/main/java/com/example/demo/controller/InicioController.java
@@ -1,2 +1,43 @@
-package com.example.demo.controller;public class InicioController {
+package com.example.demo.controller;
+
+import com.example.demo.entities.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping(value={"/"},method = RequestMethod.GET)
+public class InicioController {
+
+
+    final UserRepository userRepository;
+
+    public InicioController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @GetMapping(value = {"/", "/inicio"})
+    public String inicio() {
+        return "inicio";
+    }
+
+    @PostMapping("/validarusuario")
+    public String validarUsuario(Model model,@RequestParam("email") String email, @RequestParam("password") String password) {
+
+
+        Optional<User> optUser = Optional.ofNullable((userRepository.buscarUsuario(email, password)));
+
+        if (optUser.isPresent()) {
+            User user1 = optUser.get();
+            model.addAttribute("usuario", user1);
+            return "homePage";
+        } else {
+            return "redirect:/inicio";
+        }
+
+    }
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package com.example.demo.repository;public interface UserRepository {
+package com.example.demo.repository;
+
+import com.example.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    @Query(nativeQuery = true,value = "select * from user where email=?1 and password=?2 ")
+    User buscarUsuario(String email,String password);
+
 }
